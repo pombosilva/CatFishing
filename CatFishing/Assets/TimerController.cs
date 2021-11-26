5995c6313d0b40aab872ea7db18e8e800cc3ef5d
denny285 <83502637+denny285@users.noreply.github.com>
2021-11-26 12:13:41 +0000

    Temporizador

---
+++ CatFishing/Assets/TimerController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+
+    private float elapsedTime;
+
+    private void Awake(){
+    
+        instance = this;
+    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        timeCounter.text = "Time: 02:00.00";
+        timerGoing = true;
+        elapsedTime = 120f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EndTimer()){
+            print(Time.deltaTime);
+            elapsedTime -= Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+
+            print(timePlayingStr);
+        }
+    }
+
+    public bool EndTimer(){
+        if(elapsedTime < 0f){
+        timerGoing = false;
+        } 
+        print(timerGoing);
+        return timerGoing;
+    }
+
+    public int getSeconds(){
+        return (int)elapsedTime;
+    }
+
+}
