a86fc2e009d3d891c67fe9e511da1a5f46999eb2
Afonso <afonsodev@outlook.com>
2021-12-09 09:08:15 +0000

    Scene EndMenu criada - Pseudo Esc

--- CatFishing/Assets/Scripts/PauseMenu/PauseMenu.cs
+++ CatFishing/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -8,15 +8,19 @@ public class PauseMenu : MonoBehaviour {
 [SerializeField] GameObject pauseMenu;
 [SerializeField] GameObject controls;
 
+private bool gamePaused = false;
+
 
     public void Pause() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        gamePaused = true;
     }
 
     public void Resume() {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        gamePaused = false;
     }
 
     public void GoToScene(int sceneID) {
@@ -34,5 +38,16 @@ public class PauseMenu : MonoBehaviour {
         if(sceneID == 0)
             pauseMenu.SetActive(true);
         controls.SetActive(false);
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(!gamePaused) {
+                Pause();
+            }
+            else {
+                Resume();
+            }
+        }
     }
 }
